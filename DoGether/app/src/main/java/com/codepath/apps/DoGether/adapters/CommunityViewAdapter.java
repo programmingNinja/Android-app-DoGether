6dc90a24717d091f871ea46994749a29995b3c30
msamant <msamant@walmartlabs.com>
2015-11-07 15:34:03 -0800

    updatated view for Community landing page

--- DoGether/app/src/main/java/com/codepath/apps/DoGether/adapters/CommunityViewAdapter.java
+++ DoGether/app/src/main/java/com/codepath/apps/DoGether/adapters/CommunityViewAdapter.java
@@ -1,9 +1,16 @@
 package com.codepath.apps.DoGether.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.codepath.apps.DoGether.R;
 import com.codepath.apps.DoGether.models.CommunityView;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -14,4 +21,26 @@ public class CommunityViewAdapter extends ArrayAdapter<CommunityView> {
     public CommunityViewAdapter(Context context, int resource, List<CommunityView> objects) {
         super(context, resource, objects);
     }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        CommunityView communityView = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.community_user, parent, false);
+        }
+        // Lookup view for data population
+        ImageView ivComUserPhoto = (ImageView)convertView.findViewById(R.id.ivComUserPhoto);
+        TextView tvComUserUsername = (TextView) convertView.findViewById(R.id.tvComUserUsername);
+        TextView tvComUserEvent = (TextView)convertView.findViewById(R.id.tvComUserEvent);
+
+        Picasso.with(getContext()).load(communityView.getComUserUserPhoto()).fit().placeholder(R.drawable.abc_spinner_mtrl_am_alpha).into(ivComUserPhoto);
+        tvComUserUsername.setText(String.valueOf(communityView.getComUserUserName()));
+        tvComUserEvent.setText(String.valueOf(communityView.getComUserEventText()));
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
 }
--- DoGether/app/src/main/java/com/codepath/apps/DoGether/models/CommunityView.java
+++ DoGether/app/src/main/java/com/codepath/apps/DoGether/models/CommunityView.java
@@ -4,4 +4,33 @@ package com.codepath.apps.DoGether.models;
  * Created by msamant on 11/7/15.
  */
 public class CommunityView {
+
+ String comUserUserName;
+ String comUserEventText;
+
+    public String getComUserUserName() {
+        return comUserUserName;
+    }
+
+    public void setComUserUserName(String comUserUserName) {
+        this.comUserUserName = comUserUserName;
+    }
+
+    public String getComUserEventText() {
+        return comUserEventText;
+    }
+
+    public void setComUserEventText(String comUserEventText) {
+        this.comUserEventText = comUserEventText;
+    }
+
+    public String getComUserUserPhoto() {
+        return comUserUserPhoto;
+    }
+
+    public void setComUserUserPhoto(String comUserUserPhoto) {
+        this.comUserUserPhoto = comUserUserPhoto;
+    }
+
+    String comUserUserPhoto;
 }
